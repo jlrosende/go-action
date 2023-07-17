37e06c76379d19afd66536708ade633772bf59b9
Jorge Lopez Rosende <jorgelopezrosende@hotmail.com>
2023-07-17 17:03:19 +0000

    Set action base in js

---
+++ ci-cd/run_action.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+
+func main() {
+	if err := run_action(context.Background()); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func run_action(ctx context.Context) error {
+	fmt.Println("Test binary installation")
+
+	// define build matrix
+	oses := []string{"linux", "darwin", "windows"}
+	arches := []string{"amd64", "arm64"}
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	// get reference to the local project
+	src := client.Host().Directory(".")
+
+	// get `node` image
+	node := client.Container().From("node:16")
+
+	// mount cloned repository into `node` image
+	node = node.WithDirectory("/src", src).WithWorkdir("/src")
+	
+	node = node.WithDirectory("/src", src).WithWorkdir("/src")
+
+	node = node.WithExec([]string{"npm", "install"})
+
+	for _, runner_os := range oses {
+		for _, runner_arch := range arches {
+			fmt.Println(runner_os, runner_arch)
+		}
+	}
+
+	return nil
+}
